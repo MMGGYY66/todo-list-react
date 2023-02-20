38e50f19f826095b507e9e9746e960d9329ad80d
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-20 09:20:11 -0600

    Add a new folder components and create inside it my first components ToDoApp.js

---
+++ src/components/TodoApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoApp = () => (
+  <div>
+    <h1>Hello world!</h1>
+    <p>I am in a React Component!</p>
+  </div>
+);
+
+export default TodoApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import TodoApp from './components/TodoApp';
+
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<TodoApp />);
